1c6e898d0f14f5878ff8e6562b23264bba497e66
ArvinYang1925 <arvinyang1925@gmail.com>
2025-04-28 18:23:51 +0800

    refactor: 💡 rename user files to auth for better semantics
    renamed userValidationSchemas, userRoutes and userController to auth
    prefix for better representation of their authentication functionality

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { getTodos, createTodo, updateTodo, deleteTodo } from "../controllers/todoController";
+import { isAuth } from "../middleware/isAuth";
+import { isStudent } from "../middleware/isStudent";
+import { isInstructor } from "../middleware/isInstructor";
 
 const router = Router();
 
-router.get("/", getTodos);
+router.get("/", isAuth, isStudent, getTodos);
 router.post("/", createTodo);
 router.put("/:id", updateTodo);
-router.delete("/:id", deleteTodo);
+router.delete("/:id", isAuth, isInstructor, deleteTodo);
 
 export default router;
