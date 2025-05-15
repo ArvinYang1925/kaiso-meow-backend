56a182a8ec60dc2ba7c345732fab292aac9ecb52
DinDin <peter779701@gmail.com>
2025-05-15 19:57:48 +0800

    feat: 🎸 add updateCourseByInstructor
    新增 更新課程API

--- src/controllers/instructorCourseController.ts
+++ src/controllers/instructorCourseController.ts
@@ -1,12 +1,12 @@
 import { Response, NextFunction } from "express";
-import { CreateCourseSchema } from "../validator/courseVaildationschema";
+import { createCourseSchema, updateCourseSchema } from "../validator/courseVaildationschema";
 import { AppDataSource } from "../config/db";
 import { Course } from "../entities/Course";
 import { AuthRequest } from "../middleware/isAuth";
 
 export async function createCourse(req: AuthRequest, res: Response, next: NextFunction) {
   try {
-    const result = CreateCourseSchema.safeParse(req.body);
+    const result = createCourseSchema.safeParse(req.body);
     if (!result.success) {
       const err = result.error.errors[0];
       res.status(400).json({ status: "failed", message: err.message });
@@ -72,6 +72,54 @@ export async function getCourseDetailByInstructor(req: AuthRequest, res: Respons
     res.status(200).json({
       status: "success",
       message: "課程資訊取得成功",
+      data: course,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function updateCourseByInstructor(req: AuthRequest, res: Response, next: NextFunction) {
+  try {
+    const result = updateCourseSchema.safeParse(req.body);
+    if (!result.success) {
+      const err = result.error.errors[0];
+      res.status(400).json({ status: "failed", message: err.message });
+      return;
+    }
+
+    const userId = req.user?.id;
+    const courseId = req.params.id;
+
+    const courseRepo = AppDataSource.getRepository(Course);
+    const course = await courseRepo.findOne({ where: { id: courseId } });
+
+    if (!course) {
+      res.status(404).json({ status: "failed", message: "找不到指定課程" });
+      return;
+    }
+
+    if (course.instructorId !== userId) {
+      res.status(403).json({ status: "failed", message: "權限不足" });
+      return;
+    }
+
+    const { title, subtitle, description, highlight, duration, price, isFree, coverUrl } = result.data;
+
+    course.title = title;
+    course.subtitle = subtitle;
+    course.description = description;
+    course.highlight = highlight;
+    course.duration = duration;
+    course.isFree = isFree;
+    course.price = isFree ? 0 : price;
+    course.coverUrl = coverUrl;
+
+    await courseRepo.save(course);
+
+    res.status(200).json({
+      status: "success",
+      message: "課程更新成功",
       data: course,
     });
   } catch (err) {
