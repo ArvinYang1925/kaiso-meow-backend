a0a49cece5641813629ce02e55cc6040ce66ff7a
DinDin <peter779701@gmail.com>
2025-05-17 01:00:58 +0800

    feat: 🎸 add getCouponsByInstructor
    新增 查看講師的折扣碼

--- src/controllers/instructorCouponController.ts
+++ src/controllers/instructorCouponController.ts
@@ -3,6 +3,7 @@ import { createCouponSchema } from "../validator/couponVaildationschema";
 import { AppDataSource } from "../config/db";
 import { Coupon } from "../entities/Coupon";
 import { AuthRequest } from "../middleware/isAuth";
+import { paginationSchema } from "../validator/commonValidationSchemas";
 
 /**
  * API #47 POST - /api/v1/instructor/coupons
@@ -49,6 +50,53 @@ export async function createCoupon(req: AuthRequest, res: Response, next: NextFu
     res.status(200).json({
       status: "success",
       message: "折扣碼新增成功",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+/**
+ * API #46 GET -/api/v1/instructor/coupons?page=1&pageSize=10
+ *
+ * 📘 [API 文件 Notion 連結](https://www.notion.so/GET-api-v1-instructor-coupons-page-1-pageSize-10-1d06a246851880608bc4d9ab8a82c638?pvs=4)
+ *
+ * 此 API 讓講師可查看折扣碼
+ */
+export async function getCouponsByInstructor(req: AuthRequest, res: Response, next: NextFunction) {
+  try {
+    const parsed = paginationSchema.safeParse(req.query);
+    if (!parsed.success) {
+      const err = parsed.error.errors[0];
+      res.status(400).json({
+        status: "failed",
+        message: err.message,
+      });
+      return;
+    }
+
+    const { page, pageSize } = parsed.data;
+    const skip = (page - 1) * pageSize;
+
+    const [data, total] = await AppDataSource.getRepository(Coupon).findAndCount({
+      where: {}, // TODO: 可加入 instructorId 過濾
+      order: { createdAt: "DESC" },
+      skip,
+      take: pageSize,
+      select: ["id", "couponName", "type", "code", "value", "expiresAt"],
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        couponList: data,
+        pagination: {
+          currentPage: page,
+          pageSize,
+          totalPages: Math.ceil(total / pageSize),
+          totalItems: total,
+        },
+      },
     });
   } catch (error) {
     next(error);
