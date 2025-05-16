d95fa8ec36fb232ec62df3739576837dc4b192d5
DinDin <peter779701@gmail.com>
2025-05-16 22:47:09 +0800

    feat: 🎸 add getInstructorOrders api
    新增講師查看學生訂單API

--- src/routes/instructorRoutes.ts
+++ src/routes/instructorRoutes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { getMe, updateMe } from "../controllers/instructorController";
+import { getInstructorOrders } from "../controllers/instructorOrdersController";
+
 import { isInstructor } from "../middleware/isInstructor";
 import { isAuth } from "../middleware/isAuth";
 
@@ -7,5 +9,6 @@ const router = Router();
 
 router.get("/me", isAuth, isInstructor, getMe);
 router.put("/me", isAuth, isInstructor, updateMe);
+router.get("/orders", isAuth, isInstructor, getInstructorOrders);
 
 export default router;
