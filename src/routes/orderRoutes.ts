76912abcc22442875c67c7f369fdebb38c21bb21
JanetLai <show861031@gmail.com>
2025-05-17 09:44:29 +0800

    feat: 🎸 API No15.16
    調整縮排

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { previewOrder, getOrders, createOrder } from "../controllers/orderController";
+import { previewOrder, getOrders, createOrder, getOrderDetail, applyCoupon } from "../controllers/orderController";
 import { isAuth } from "../middleware/isAuth";
 import { isStudent } from "../middleware/isStudent";
 
@@ -8,4 +8,7 @@ const router = Router();
 router.post("/preview", isAuth, isStudent, previewOrder);
 router.get("/", isAuth, isStudent, getOrders);
 router.post("/", isAuth, isStudent, createOrder);
+router.get("/:orderId", isAuth, isStudent, getOrderDetail);
+router.post("/:orderId/apply-coupon", isAuth, isStudent, applyCoupon);
+
 export default router;
