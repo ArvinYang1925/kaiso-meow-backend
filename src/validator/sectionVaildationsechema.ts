64b90d1e04099f13c89421419171daa6bba462a0
DinDin <peter779701@gmail.com>
2025-05-22 22:36:24 +0800

    fix: 🐛 fix to add error message
    zod判定加入type錯誤訊息

--- src/validator/sectionVaildationsechema.ts
+++ src/validator/sectionVaildationsechema.ts
@@ -2,7 +2,10 @@
 import { z } from "zod";
 
 export const sectionSchema = z.object({
-  title: z.string().min(1, "章節標題為必填").max(100, "章節標題不得超過 100 字"),
+  title: z
+    .string({ required_error: "title 是必填欄位", invalid_type_error: "title 必須是文字" })
+    .min(1, "章節標題為必填")
+    .max(100, "章節標題不得超過 100 字"),
 });
 
 export const updateSectionSchema = z.object({
